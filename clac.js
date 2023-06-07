02e582dedd366fb29f201500378510ef0a5cfc81
mahesh shetty <119321614+Maheshshetty-123@users.noreply.github.com>
2023-06-07 20:42:26 +0530

    Add files via upload

---
+++ clac.js
@@ -0,0 +1,22 @@
+let result=document.getElementById("inputext");
+
+let calculate=(number)=>{
+result.value=result.value+number;
+}
+
+let  Result=()=>{
+try{
+    result.value=eval(result.value)
+}
+catch(err){
+alert("enter the valid input")
+}
+}
+
+function clr(){
+    result.value="0";
+}
+
+function del(){
+result.value=result.value.slice(0,-1);
+}
